21626ba97f37d5bcf1acb1a0f1bc93aba4ac117a
Gdvrty005 <Avinash.godavarthi7@gmail.com>
2018-01-20 11:09:07 +0000

    spring boot code

---
+++ src/main/java/com/bountyhunters/calculator/controller/CalculatorController.java
@@ -0,0 +1,16 @@
+package com.bountyhunters.calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+	
+	@GetMapping(value="add")
+	public String add(@RequestParam(name="num1")int int1,@RequestParam(name="num2")int int2){
+		int i = int1+int2;
+		return "Sum of two numbers is:"+i;
+	}
+
+}
